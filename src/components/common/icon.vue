90e0128da09d96b2f1036c357284ebe508797d50
liuyun <atme_talk@163.com>
2016-11-30 18:49:57 +0800

    icon组件

---
+++ src/components/common/icon.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="icon-wrap" :style="wrapStyle">
+        <div :style="iconStyle">
+            
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['icon', 'wrapColor', 'iconColor'],
+        data() {
+            return {
+                wrapStyle: {
+                    backgroundColor: this.wrapColor
+                },
+                iconStyle: {
+                    background: `url(${this.icon}) 0 0 no-repeat`,
+                    backgroundSize: 'cover',
+                    filter: `drop-shadow(30px 0 0 ${this.iconColor})`
+                }
+            }
+        },
+    }
+</script>
+<style scoped>
+    .icon-wrap {
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        overflow: hidden;
+    } 
+    .icon-wrap div {
+        width: 20px;
+        height: 20px;
+        margin-top: 7px;
+        margin-left: -22px; 
+        border-right: 30px solid transparent; 
+    } 
+</style>
