023bd539ff3f571a242fd79a6c4816b8bcc4a0f2
liuyun <atme_talk@163.com>
2016-12-01 15:09:37 +0800

    floder component 开发

--- src/components/common/floder.vue
+++ src/components/common/floder.vue
@@ -7,46 +7,128 @@
         </div>
 
         <div class="floder-input">
-            <icon class="custom" :icon="currentIcon" wrapColor="#315162" iconColor="#00c0ff"></icon>
+            <icon class="custom" :icon="currentIcon.icon" wrap-color="#315162" :icon-color="currentIcon.iconColor">
+            </icon>
             <input type="text" name="" value="hah">
         </div>
 
 
-        <div class="icons-toggle">
-            <p>隐藏图标</p>
+        <div class="icons-toggle" @click="showIconsTog">
+            <p>{{ infro }}图标</p>
         </div>
 
-        <div class="floder-icons">
-            <div class="icons-select">
-                <button type="button" class="file active">文件夹图标</button>
-                <button type="button" class="kind">分类图标</button>
+        <div class="floder-icons" v-show="showIcons">
+            <div class="icons-select" @click.stop.prevent="togHandle">
+                <button type="button" class="file" :class="{active: isActive}">文件夹图标</button>
+                <button type="button" class="kind" :class="{active: !isActive}">分类图标</button>
             </div>
-            <ul class="icon-list" v-show="condition">
-                <li v-for="item of items" class="iconItem">
-                    <icon wrapColor="#315162" iconColor="#fff" :icon="currentIcon"></icon>
+            <ul class="icon-list" v-show="iconPanelToggle">
+                <li v-for="item of fileIcons" class="iconItem" @click.stop="iconClickHandle(item)">
+                    <icon wrap-color="#315162" :icon-color="item.iconColor" :icon="item.icon">
+                    </icon>
                 </li>
             </ul>
-            <ul class="icon-list" v-show="!condition">
-
+            <ul class="icon-list" v-show="!iconPanelToggle">
+                <li v-for="item of kindIcons" class="iconItem" @click.stop="iconClickHandle(item)">
+                    <icon wrap-color="#315162" :icon-color="item.iconColor" :icon="item.icon">
+                    </icon>
+                </li>
             </ul>
+        </div>
+
+        <div class="floder-delete">
+            <icon 
+                v-show="editing"
+                class="del-icon" 
+                :icon="deleteIcon" 
+                icon-color="red" 
+                wrap-color="#3a5f73"
+                ></icon>
         </div>
     </div>
 </template>
 <script>
     import file from '../../assets/file.png'
 
+    import del from '../../assets/delete.png'
+
+    const kindPic = [
+        require('../../assets/HTML.png'),
+        require('../../assets/css.png'),
+        require('../../assets/javascript.png'),
+        require('../../assets/jquery.png'),
+        require('../../assets/github2.png'),
+        require('../../assets/sublime.png'),
+        require('../../assets/github4.png'),
+        require('../../assets/github5.png'),
+        require('../../assets/program.png'),
+        require('../../assets/vue.png'),
+        require('../../assets/react.png'),
+        require('../../assets/angular.png'),        
+    ]
+    const kindIcons = kindPic.map((icon) => (
+        {
+            iconColor: '#fff',
+            icon: icon
+        }
+    ))
+
     const fileIcons = [
-        {iconColor:'#fff'},
-        {iconColor:'#fff'},
-        {iconColor:'#fff'},
-        {iconColor:'#fff'},
+        {icon: file, iconColor: '#fff'},
+        {icon: file, iconColor: '#ba5db9'},
+        {icon: file, iconColor: '#bda0bd'},
+        {icon: file, iconColor: '#3cad49'},
+        {icon: file, iconColor: '#14501b'},
+        {icon: file, iconColor: '#ff8a65'},
+        {icon: file, iconColor: '#ccc929'},
+        {icon: file, iconColor: '#1ad7ff'},
+        {icon: file, iconColor: '#d77a41'},
+        {icon: file, iconColor: '#1b1b1b'},
+        {icon: file, iconColor: '#996545'},
+        {icon: file, iconColor: '#66aacc'},        
     ]
     export default {
+        props:['editing'],
         data() {
             return {
-                currentIcon: file,
-                condition: true,
-                items: fileIcons
+                currentIcon: {
+                    icon: file,
+                    iconColor: '#fff'
+                },
+                deleteIcon: del,
+                infro: '隐藏', 
+                isActive: true,   
+                showIcons: true,
+                iconPanelToggle: true,
+                kindIcons: kindIcons,
+                fileIcons: fileIcons
+            }
+        },
+        methods: {
+            iconClickHandle: function({ icon, iconColor }) {
+                this.currentIcon = {
+                    icon: icon,
+                    iconColor: iconColor
+                }
+            },
+            togHandle: function(e) {
+                const tar = e.target;
+                if (tar.className.split(' ')[0] === 'kind') {
+                    this.isActive = false
+                    this.iconPanelToggle = false
+                } else {
+                    console.log('1')
+                    this.isActive = true
+                    this.iconPanelToggle = true
+                }    
+            },
+            showIconsTog: function() {
+                this.showIcons = !this.showIcons
+                if(this.showIcons === true) {
+                    this.infro = '隐藏'
+                } else {
+                    this.infro = '显示'
+                }
             }
         },
         components: {
@@ -57,9 +139,10 @@
 <style scoped>
     .floder {
         width: 300px;
-       border-radius: 8px;
+        border-radius: 8px;
         background: #3a5f73;
     }
+    
     .floder-header {
         position: relative;
         width: 100%;
@@ -125,7 +208,8 @@
         height: 70px;
         background: #3a5f73;
     }
-    .icons-toggle{
+    
+    .icons-toggle {
         height: 30px;
         line-height: 30px;
         color: #d9d9d9;
@@ -134,14 +218,17 @@
         text-indent: 21px;
         cursor: pointer;
     }
+    
     .icons-toggle:hover {
         color: #c9c9c9
     }
+    
     .icons-select {
         width: 258px;
         margin: 8px auto;
         font-size: 0px;
     }
+    
     .icons-select button {
         display: inline-block;
         height: 28px;
@@ -149,20 +236,40 @@
         font-size: 12px;
         color: #c9c9c9;
         background: #3a5f73;
+        outline: none;
+        cursor: pointer;
     }
-
+    
     .file {
         border: 1px solid #315162;
         border-right: none;
         border-radius: 5px 0 0 5px;
     }
+    
     .kind {
         border: 1px solid #315162;
         border-left: none;
         border-radius: 0 5px 5px 0;
     }
-    .iconItem{
-        float: left;
+    
+    .icon-list {
+        padding: 0 15px;
     }
-
+    
+    .iconItem {
+        display: inline-block;
+        margin: 10px 16px;
+    }
+    .active {
+        background: #315162 !important;
+    }
+    .floder-delete {
+        position: relative;
+        width: 100%;
+        height: 40px;
+    }
+    .del-icon {
+        position: absolute;
+        right: 10px;
+    }
 </style>
