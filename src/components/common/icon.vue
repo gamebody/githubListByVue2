d7a1a8dceaf4e9abba2b1dc0eeae80b4709d48e0
liuyun <atme_talk@163.com>
2016-12-01 15:08:27 +0800

    更改一些icon组件的bug

--- src/components/common/icon.vue
+++ src/components/common/icon.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="icon-wrap" :style="wrapStyle">
+    <div class="icon-contanier" :style="wrapStyle">
         <div :style="iconStyle">
-            
+
         </div>
     </div>
 </template>
@@ -12,28 +12,33 @@
             return {
                 wrapStyle: {
                     backgroundColor: this.wrapColor
-                },
-                iconStyle: {
-                    background: `url(${this.icon}) 0 0 no-repeat`,
-                    backgroundSize: 'cover',
-                    filter: `drop-shadow(30px 0 0 ${this.iconColor})`
                 }
             }
         },
+        computed: {
+            iconStyle: function() {
+                return {
+                    background: `url(${this.icon}) 0 0 /cover no-repeat`,
+                    filter: `drop-shadow(30px 0 0 ${this.iconColor})`
+                }
+            }
+        }
     }
 </script>
 <style scoped>
-    .icon-wrap {
+    .icon-contanier {
         width: 35px;
         height: 35px;
         border-radius: 50%;
         overflow: hidden;
-    } 
-    .icon-wrap div {
+        cursor: pointer;
+    }
+    
+    .icon-contanier div {
         width: 20px;
         height: 20px;
         margin-top: 7px;
-        margin-left: -22px; 
-        border-right: 30px solid transparent; 
-    } 
+        margin-left: -22px;
+        border-right: 30px solid transparent;
+    }
 </style>
